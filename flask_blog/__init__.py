8a5bda648c7c7ba38052c5358129a9d2e2232b2f
Ibrahim Tahir <ibrahimtobi145@gmail.com>
2025-07-30 14:36:55 +0100

    add error handling templates and update .env file with credentials

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -27,8 +27,11 @@ def create_app(config_class = config):
     from flask_blog.posts.routes import posts
     from flask_blog.main.routes import main
 
+    from flask_blog.errors.handlers import errors
+
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
